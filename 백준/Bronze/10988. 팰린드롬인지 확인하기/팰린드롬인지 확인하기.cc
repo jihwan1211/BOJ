365383d04cf86512773692da526cf380a9ad4555
jihwan1211 <129527258+jihwan1211@users.noreply.github.com>
2023-07-05 23:38:01 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -6,26 +6,11 @@ int main(void){
    cin.tie(NULL);
    cout.tie(NULL);
 
-   string s;
-   stack<char> st;
-   bool flag=true;
+   string s, temp;
    cin>>s;
-   for(int i=0; i<s.size(); i++){
-     if(i<s.size()/2) {
-         st.push(s[i]);
-         continue;
-      }
-     if(s.size()%2==1 && i==s.size()/2) continue;
-     else{
-      if(st.top()!=s[i]) {
-         flag=false;
-         break;
-      }else{
-         if(st.size()) st.pop();
-      }
-     }
-   }
-
-  cout<<flag;
-  return 0;
+   temp=s;
+   reverse(temp.begin(), temp.end());
+   if(s==temp) cout<<1;
+   else cout<<0;
+   return 0;
 }

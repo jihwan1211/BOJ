50f0ea66624fa4e7a5804bc25604b2b13b567b0e
김지환 <129527258+jihwan1211@users.noreply.github.com>
2023-10-16 16:09:22 +0900

    [Gold I] Title: 동전 뒤집기, Time: 668 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -4,14 +4,12 @@ using namespace std;
 int n, ret=987654321;
 int arr[21][21];
 int row[21];
-int col[21];
 
 void go(){
     int num=0;
     int temp[21][21];
     memcpy(temp, arr, sizeof(arr));
     for(int i=0; i<n; i++){
-        // int cnt=row[i];
         if(row[i]) {
             for(int j=0; j<n; j++){
                 if(temp[i][j]) temp[i][j]=0;
@@ -29,12 +27,10 @@ void go(){
                 if(temp[j][i]) temp[j][i]=0;
                 else temp[j][i]=1;
             }
+            num+=n-t;
+            continue;
         }
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(!temp[i][j]) num++;
-        }
+        num+=t;
     }
     ret=min(ret, num);
 }
@@ -53,71 +49,12 @@ int main(void){
         }
     }
 
-    // for(int i=0; i<n; i++){
-    //     int cnt=0;
-    //     for(int j=0; j<n; j++){
-    //         if(!arr[i][j]) cnt++;
-    //     }
-    //     row[i]=cnt;
-    // }
-
-    // for(int i=0; i<n; i++){
-    //     if(n/2 >= row[i]) continue;
-    //     else{
-    //         for(int j=0; j<n; j++){
-    //             if(arr[i][j]) arr[i][j]=0;
-    //             else arr[i][j]=1;
-    //         }
-    //     }
-    // }
-
-    // for(int i=0; i<n; i++){
-    //     int cnt=0;
-    //     for(int j=0; j<n; j++){
-    //         if(arr[j][i]) cnt++;
-    //     }
-    //     col[i]=cnt;
-    // }
-
-    // for(int i=0; i<n; i++){
-    //     if(n/2 >= col[i]) continue;
-    //     else{
-    //         for(int j=0; j<n; j++){
-    //             if(arr[i][j]) arr[i][j]=0;
-    //             else arr[i][j]=1;
-    //         }
-    //     }
-    // }
-
-    // int res=0;
-
-    // for(int i=0; i<n; i++){
-    //     for(int j=0; j<n; j++){
-    //         if(!arr[i][j]) res++;
-    //     }
-    // }
-    // cout<<res;
-
     for(int i=0; i<(1<<n); i++){
         for(int k=0; k<n; k++){
             if(i&(1<<k)) row[k]=1;
         }
         go();
         memset(row, 0, sizeof(row));
-        // for(int j=0; j<(1<<n); j++){
-        //     for(int k=0; k<n; k++){
-        //         // 열에서 켜있는 비트 찾기
-        //         if(i&(1<<k)){
-        //             row[k]=1;
-        //         }
-        //         if(j&(1<<k)){
-        //             col[k]=1;
-        //         }
-        //     }
-        //     go();
-        //     memset(row, 0, sizeof(row));
-        //     memset(col, 0, sizeof(col));
-        // }
     }
     cout<<ret;
     return 0;

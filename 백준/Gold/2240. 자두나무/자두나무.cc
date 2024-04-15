789fd331a8938f071f4e6d554aaa237d85c99fee
김지환 <129527258+jihwan1211@users.noreply.github.com>
2024-04-15 10:22:26 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2148 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -5,21 +5,31 @@ int t, w, ret;
 int arr[32][1004];
 vector<int> vec;
 
-void go(){ 
-    for(int i = 0; i <= w; i++){
-        for(int j = 0; j < t; j++){
-            int tree_pos = (i % 2) + 1; // 현재 자두의 위치 (i번 움직였다면 어느 나무 아래에 있는가)
-            int ja_cnt = (vec[j] == tree_pos) ? 1 : 0; // 현재 초에 떨어지는 자두를 받을 수 있다면 1, 아니면 0
+void go(){
+    //arr[w][t] t초이고, w번 움직였을때 받는 자두의 최대
+    for(int i=0; i<=w; i++){
+        for(int j=0; j<t; j++){
+            int temp_pos=0;
+            int ja_cnt=0;
+            if(i%2 == 0){
+                temp_pos = 1;
+                if(vec[j] == temp_pos) ja_cnt=1;
+            }else{
+                temp_pos = 2;
+                if(vec[j] == temp_pos) ja_cnt=1;
+            }
 
-            if(j == 0){
-                arr[i][j] = ja_cnt; // 첫 초에는 이전 값이 없으므로 현재 값만 저장
-            } else if(i == 0){
-                arr[i][j] = arr[i][j-1] + ja_cnt; // 움직이지 않는 경우, 이전 초의 값에 현재 초의 결과를 더함
-            } else {
-                arr[i][j] = max(arr[i-1][j-1], arr[i][j-1]) + ja_cnt; // 움직이거나, 움직이지 않는 경우 중 최대값 선택 후 현재 초의 결과를 더함
+            if(i == 0){
+                if(j != 0) arr[i][j] = arr[i][j-1]+ja_cnt;
+                else arr[i][j] = ja_cnt;
+            }else{
+                if(j != 0) arr[i][j] = max(arr[i-1][j-1], arr[i][j-1]) + ja_cnt;
+                // else arr[i][j] = arr[i-1][j] + ja_cnt; // 여기가 틀린 거 같네
+                else arr[i][j] = ja_cnt; // 여기가 틀린 거 같네
             } 
         }
     }
+    return;
 }
 
 int main(void){
@@ -27,18 +37,18 @@ int main(void){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> t >> w;
-    vec.resize(t);
-    for(int i = 0; i < t; i++){
-        cin >> vec[i]; 
+    cin>>t>>w;
+
+    for(int i=0; i<t; i++){
+        int temp;
+        cin>>temp;
+        vec.push_back(temp);
     }
-
-    go(); 
-
-    for(int i = 0; i <= w; i++) {
-        ret = max(ret, arr[i][t-1]);  
+    go();
+    for(int i=0; i<=w; i++) {
+        ret=max(ret, arr[i][t-1]);
     }
-
-    cout << ret; 
+    
+    cout<<ret;
     return 0;
 }

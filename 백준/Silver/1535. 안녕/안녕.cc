efd5a6c7f0797911a82277029261615016f087d9
김지환 <129527258+jihwan1211@users.noreply.github.com>
2024-04-24 12:22:43 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int L[21], J[21], n;
+int dp[104];
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>L[i];
+    }
+
+    for(int i=0; i<n; i++){
+        cin>>J[i];
+    }
+
+    for(int i=0; i<20; i++){
+        for(int hp=100; hp>L[i]; hp--){
+            if(hp-L[i]<=0) continue;
+            dp[hp] = max(dp[hp], dp[hp-L[i]] + J[i]);
+        }
+    }
+
+    cout<<dp[100];
+    return 0;
+}

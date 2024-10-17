87052ff3adb4409e6fb595c6522921da59da9a0d
김지환 <129527258+jihwan1211@users.noreply.github.com>
2024-10-17 15:02:14 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5948 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -2,27 +2,7 @@
 using namespace std;
 
 string str1, str2;
-int dp[1004][1004];
-
-void go(){
-    
-    for(int i=0; i<str2.length(); i++){
-        if(str1[0] == str2[i]) dp[0][i] = 1;
-        if(i>0 && dp[0][i-1]) dp[0][i] = 1;
-    }
-
-    for(int i=1; i<str1.length(); i++){
-        for(int j=0; j<str2.length(); j++){
-            if(j==0){
-                if(str1[i] == str2[j]) dp[i][j] = 1;
-                else dp[i][j] = dp[i-1][j];
-            }else{
-                if(str1[i] == str2[j]) dp[i][j] =dp[i-1][j-1] +1;
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-    }
-}
+int dp[1002][1002];
 
 int main(void){
     ios_base::sync_with_stdio(false);
@@ -30,8 +10,32 @@ int main(void){
     cout.tie(NULL);
 
     cin>>str1>>str2;
+    for(int i=0; i<str1.length(); i++){
+        for(int j=0; j<str2.length(); j++){
+            if(j==0){
+                if(str1[i] == str2[j]) dp[i][j] = 1;
+                else{
+                    if(i!=0) dp[i][j] = dp[i-1][j];
+                    else dp[i][j] = 0;
+                }
+            }else{
+                if(str1[i] == str2[j]){
+                    if(i==0) dp[i][j] = 1;
+                    else{
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    }
+                }else{
+                    if(i==0) dp[i][j] = dp[i][j-1];
+                    else{
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+        }
+    }
 
-    go();
+
     cout<<dp[str1.length()-1][str2.length()-1];
+
     return 0;
 }

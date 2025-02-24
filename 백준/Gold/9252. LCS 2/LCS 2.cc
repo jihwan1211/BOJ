bde6c6191bc70ffbc3507026ad8cab43ce995973
김지환 <129527258+jihwan1211@users.noreply.github.com>
2025-02-24 12:49:53 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,98 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// https://www.acmicpc.net/problem/9252
-int arr[1024][1024];
-string str1, str2;
-
-int main(void){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin>>str1>>str2;
+    string str1, str2;
+    cin >> str1 >> str2;
 
-    for(int i=0; i<str1.length(); i++){
-        for(int j=0; j<str2.length(); j++){
-            if(i==0){
-                if(j==0) {
-                    if(str1[i] == str2[j]) arr[i][j] = 1;
-                    else arr[i][j] = 0;
-                }else{
-                    if(str1[i] == str2[j]) arr[i][j] = 1;
-                    else arr[i][j] = arr[i][j-1];
-                }
-            }else{
-                if(j==0) {
-                    if(str1[i] == str2[j]) arr[i][j] = 1;
-                    else arr[i][j] = arr[i-1][j];
-                }else{
-                    if(str1[i] == str2[j]) arr[i][j] = arr[i-1][j-1] + 1;
-                    else arr[i][j] = max(arr[i-1][j], arr[i][j-1]);
-                }
+    int n = str1.size(), m = str2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    // LCS 길이 계산
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
 
-    // for(int i=0; i<str1.length(); i++){
-    //     for(int j=0; j<str2.length(); j++){
-    //         cout<<arr[i][j]<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
-    // cout<<"\n";
+    cout << dp[n][m] << "\n"; // LCS 길이 출력
 
-    queue<char> q;
-    int row = str1.length() - 1;
-    int col = str2.length() - 1;
-    while(true){
-        // row,  col이 범위 내에 있는지...
-        if(row < 0 || col < 0) break;
-        // 단어가 같은지 확인
-        if(str1[row] == str2[col]){
-            q.push(str2[col]);
-            row--;
-            col--;
-            continue;
-        }else{
-            // 단어가 달라
-            if(row == 0 && col == 0) break;
-            else if(row == 0 && col != 0){
-                // row가 0일때는 col만 줄이면서 확인
-                col--;
-            }else if(row != 0 && col == 0){
-                // col이 0일때는 row만 줄이면서 확인
-                row--;
-            }else{        
-                if(arr[row-1][col] > arr[row][col-1]){
-                // 위가 더 커
-                    row--;
-                    continue;
-                }else if(arr[row-1][col] < arr[row][col-1]){
-                // 옆이 더 커
-                    col--;
-                    continue;
-                }else{
-                    // 같아
-                    row--;
-                    continue;
-                }
-            }
+    // LCS 문자열 찾기 (역추적)
+    string lcs = "";
+    int i = n, j = m;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            lcs += str1[i - 1];
+            i--, j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
         }
     }
 
+    // LCS 출력 (뒤집어서)
+    if (!lcs.empty()) {
+        reverse(lcs.begin(), lcs.end());
+        cout << lcs;
+    }
 
-    string answer = "";
-    int answer_size = q.size();
-    while(q.size()){
-        answer += q.front();
-        q.pop();
-    }
-    cout<<answer_size<<"\n";
-    if(answer_size) {
-        reverse(answer.begin(), answer.end());
-        cout<<answer;
-    }
-    
     return 0;
 }
